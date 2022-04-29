663b897179d9b4fca8f4cc37194598409c1c2234
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-04-29 19:23:57 +0300

    Add initial recoil configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import type { AppProps } from "next/app";
 
 import "antd/dist/antd.min.css";
 import "../styles/globals.css";
+import { RecoilRoot } from "recoil";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 };
 
 export default MyApp;
