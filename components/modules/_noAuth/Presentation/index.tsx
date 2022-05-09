42c09e599e82cccc85db487d75eb2c616aaf0411
Jonath-z <jonathanzihindula95@gmail.com>
2022-05-09 16:19:29 +0200

    text white color now work for an y background color

--- components/modules/_noAuth/Presentation/index.tsx
+++ components/modules/_noAuth/Presentation/index.tsx
@@ -31,7 +31,7 @@ const Presentation = () => {
   };
 
   return (
-    <div className="mt-24 flex justify-center items-center 2xl:w-[80%] xl:w-[85%] lg:w-[90%] md:w-[95%] mx-auto h-fit px-5">
+    <div className="my-24 flex justify-center items-center 2xl:w-[80%] xl:w-[85%] lg:w-[90%] md:w-[95%] mx-auto h-fit px-5">
       <button
         className="z-10 w-fit h-fit rounded-full -mr-4 mobile:hidden"
         onClick={onScrollLeft}
