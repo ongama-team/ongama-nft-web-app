8460a3100827087cbb5e8961094bb7a80e27a678
Jonath-z <jonathanzihindula95@gmail.com>
2022-05-12 19:39:21 +0200

    DropPage component removed from Landing page

--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import DropPage from "../modules/_noAuth/DropPage";
 import Header from "../modules/_noAuth/Header";
 import Presentation from "../modules/_noAuth/Presentation";
 
