298f1ef195957304bfcf0be5d6b724ba2632af5b
Muhindo-Galien <muhindogalien512@gmail.com>
2022-05-25 08:49:41 -0700

    added top sellers

---
+++ components/modules/__modules__/Card/TopSellersCard/index.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
+import { formatToDollars } from "helpers/numberFormatter";
+import { dummy_data } from "../../../__noAuth/TopSellers/dummy_data";
+import { VerifiedImg } from "@lib/Resources";
+
+const TopSellersCard = () => {
+  const { topSellersData } = dummy_data;
+  return (
+    <>
+      {topSellersData.map((seller, index) => {
+        return (
+          <div
+            className=" flex-none h-14 w-[250px] bg-none flex items-center mx-1 my-2"
+            key={index}
+          >
+            <span className="text-gray-400">{index + +1}</span>
+            <div className="flex">
+              <section className="relative">
+                <img
+                  src={seller.url}
+                  alt="profile"
+                  className="h-12 w-12 rounded-full ml-4 mr-4"
+                />
+                <img
+                  src={VerifiedImg}
+                  alt="checkmark"
+                  className="h-8 absolute bottom-[-7px] right-2"
+                />
+              </section>
+
+              <section>
+                <h4 className="text-black text-sm font-bold">{seller.title}</h4>
+                <span className="text-sm font-semibold text-gray-400">
+                  {formatToDollars(seller.price)}
+                </span>
+              </section>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default TopSellersCard;
---
+++ components/modules/__noAuth/TopSellers/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import AllTopSellers from "./AllTopSellers";
+
+const TopSellers = () => {
+  return (
+    <div className="flex flex-col justify-center 2xl:w-[80%] xl:w-[85%] lg:w-[90%] md:w-[95%] mx-auto px-5 my-10">
+      <div className="font-bold text-xl text-black font-ibmPlexSans md:text-2xl lg:text-3xl">
+        Top
+        <select
+          name="days"
+          className="bg-transparent font-bold border-none outline-none text-blue-500 px-1"
+        >
+          <option defaultChecked value="1" className="text-sm border-none py-2">
+            sellers
+          </option>
+          <option value="7" className="text-black text-sm  py-2">
+            buyers
+          </option>
+        </select>{" "}
+        in
+        <select
+          name="days"
+          className="bg-transparent font-bold border-none outline-none text-blue-500 px-1"
+        >
+          <option defaultChecked value="1" className="text-sm border-none py-0">
+            1 day
+          </option>
+          <option value="7" className="text-black text-sm  py-2">
+            2 days
+          </option>
+        </select>
+        {/* <select
+          name="days"
+          className="bg-transparent font-bold border-none outline-none text-blue-500 px-0"
+        >
+          <option defaultChecked value="1" className="text-sm border-none py-2">
+            1 day
+          </option>
+          <option value="7" className="text-black text-sm  py-2">
+            7 days
+          </option>
+          <option value="30" className="text-black text-sm py-2">
+            30 days
+          </option>
+          </select> */}
+      </div>
+      <AllTopSellers />
+    </div>
+  );
+};
+
+export default TopSellers;
