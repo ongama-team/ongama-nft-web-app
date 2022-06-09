26be855ff10237206c3ab54aa97be517b0c9b722
Merdi-kim <merdikimuha1@gmail.com>
2022-06-09 06:06:09 +0300

    update verified-image url

--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -4,7 +4,7 @@ import Presentation from "../modules/__noAuth/Presentation";
 import TopCollection from "@components/modules/__noAuth/TopCollection";
 import ConnectWalletBox from "@components/modules/__noAuth/ConnectWalletsBox";
 import TopSellers from "@components/modules/__noAuth/TopSellers";
-import ProfileMenu from "@components/modules/__secured/ProfileMenu";
+import HotCollections from "@components/modules/__noAuth/HotCollections";
 
 const LandingPage = () => {
   return (
@@ -14,7 +14,7 @@ const LandingPage = () => {
       <TopCollection />
       <ConnectWalletBox />
       <TopSellers />
-      <ProfileMenu />
+      <HotCollections />
     </>
   );
 };
