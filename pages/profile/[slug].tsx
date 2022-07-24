63f091b61395f814507c18b8347f00c6a0c05b7d
Jonath-z <jonathanzihindula95@gmail.com>
2022-07-24 23:28:03 +0200

    profile page loading state added

--- pages/profile/[slug].tsx
+++ pages/profile/[slug].tsx
@@ -1,10 +1,30 @@
 import ProfileContainer from "@components/modules/__secured/Profile";
-import React from "react";
+import React, { useEffect } from "react";
 import cookieParser from "@lib/cookieParser";
 import { backendApiService } from "@lib/services/BackendApiService";
+import UploadFileProcessing from "@components/modules/__modules__/Card/UploadFileProcessing";
 
-function Profile({ user }) {
-  return <ProfileContainer searchedUserProfile={user} />;
+function Profile({ user, loaded }) {
+  if (!user && !loaded) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen w-full">
+        <h1 className="text-2xl font-bold">Failed To Load the page</h1>
+        <button
+          onClick={() => {
+            if (typeof window !== "undefined") window.location.reload();
+          }}
+          className="px-5 py-2 bg-blue-500 hover:blue-600 transition-all font-bold text-white"
+        >
+          Try again
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadFileProcessing isProcessing={!user}>
+      <ProfileContainer searchedUserProfile={user} />
+    </UploadFileProcessing>
+  );
 }
 
 export async function getServerSideProps(context: {
@@ -16,16 +36,25 @@ export async function getServerSideProps(context: {
   const headers = {
     address: cookies._anft_user_address || "",
   };
+  try {
+    const [user] = await Promise.all([
+      backendApiService.findAccountWhereAddressOrUsername(slug, headers),
+    ]);
 
-  const [user] = await Promise.all([
-    backendApiService.findAccountWhereAddressOrUsername(slug, headers),
-  ]);
-
-  return {
-    props: {
-      user: user,
-    },
-  };
+    return {
+      props: {
+        user: user,
+        loaded: true,
+      },
+    };
+  } catch (err) {
+    return {
+      props: {
+        user: null,
+        loaded: false,
+      },
+    };
+  }
 }
 
 export default Profile;
